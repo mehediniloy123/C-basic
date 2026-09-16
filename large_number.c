e1d18dacc6b2a869bb5a8899b113d87dd055f080
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-16 19:28:36 +0600

    new

---
+++ large_number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int time = 17;
+
+    if(time == 17){
+        printf("Good morning\n");
+        printf("Mehedi Hasan\n");
+    }
+    else{
+        printf("Sorry its not morining\n");
+        printf("Niloy\n");
+    }
+    return 0;
+}
