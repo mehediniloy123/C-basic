c81f4abf3f5b80f11f98347af63b07eb324e53e0
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-16 20:08:33 +0600

    new

---
+++ large_number2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter First number : ");
+    scanf("%d", &num1);
+
+    printf("Enter Second number : ");
+    scanf("%d", &num2);
+
+    if(num1>num2)
+        printf("Large = %d\n", num1);
+
+    if(num1<num2)
+        printf("Large = %d\n", num2);
+
+    return 0;
+}
