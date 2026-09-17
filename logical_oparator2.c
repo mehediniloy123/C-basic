98decac5d4306d402ebf6f754532635846b4809b
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-17 21:47:41 +0600

    read 3 number with logical oparator

---
+++ logical_oparator2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int num1, num2, num3;
+
+    printf("Enter three numberrr : ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if(num1>num2 && num1>num3)
+        printf("Largeee number = %d\n", num1);
+
+    else if(num2>num1 && num2>num3)
+        printf("Largee number = %d\n", num2);
+
+    else if(num3>num1 && num3>num2)
+        printf("Largee number = %d\n", num3);
+
+    else
+        printf("Numbers are equal");
+
+    return 0;
+}
