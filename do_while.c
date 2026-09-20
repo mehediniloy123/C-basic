9b7c0baeaa6ae9ca7da3b86903d7a415fe9701e1
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-20 20:24:18 +0600

    new

---
+++ do_while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i=1;//initialization
+    do{
+        printf("%d ",i);
+        printf("Niloy\n");
+        i++;//increment
+    }
+    while(i<=100);//condition
+
+    return 0;
+}
