ad2c73049932977d3131ea4571e869d94c0c3b70
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-21 23:33:59 +0600

    new

---
+++ multification_table.c
@@ -0,0 +1,18 @@
+/*
+    Enter any number : 3
+    num x i = num*i
+    3 x 1 = 3
+    3 x 2 = 6
+    .........
+    3 x 10 = 30
+*/
+#include <stdio.h>
+int main(){
+    int num, i;
+    printf("Enter any number : ");
+    scanf("%d", &num);
+    for(i=1; i<=10; i++){
+        printf("%d X %d = %d\n", num, i, num*i);
+    }
+    return 0;
+}
